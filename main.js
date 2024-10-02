bd35b4715e55f20a3bc6100b5b81c6cc7e9f69db
h1tax <115391278+z1rowww@users.noreply.github.com>
2024-10-02 17:16:04 +0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-let burger = document.getElementById("burger");
-let navbarlist = document.getElementById("navbarlist");
-
-burger.addEventListener("click", () => {
-  burger.classList.toggle("active");
-  navbarlist.classList.toggle("navbar-list--active");
-});
